810312fa894ad92d76c3aad683653bb9cf0d379f
Insu Park <facessu@gmail.com>
2023-06-22 12:51:55 +0900

    폴더 정리

--- DayTwoBasicTraining.playground/Pages/공배수.xcplaygroundpage/Contents.swift
+++ DayTwoBasicTraining.playground/Pages/공배수.xcplaygroundpage/Contents.swift
@@ -3,5 +3,5 @@
 import Foundation
 
 func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
-    return (number % n == 0) && (number % m == 0) ? 1 : 0 
+    return (number % n == 0) && (number % m == 0) ? 1 : 0
 }
