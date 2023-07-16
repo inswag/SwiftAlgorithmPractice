1cab017f646e9543b9f8129d3dc66f8ee0719b4e
Insu Park <facessu@gmail.com>
2023-07-16 22:28:54 +0900

    Programmers Day 15 - Coding Test Intro 10 Questions

---
+++ Programmers/DayFifteenIntroCodingTests.playground/Pages/두 수의 곱.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ Programmers Introduction to Coding Tests
+ Start from easy to hard
+ */
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 * num2
+}
---
+++ Programmers/DayFifteenIntroCodingTests.playground/Pages/두 수의 합.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 + num2
+}
