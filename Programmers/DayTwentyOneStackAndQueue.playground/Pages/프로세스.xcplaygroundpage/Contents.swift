9398e7f7a766e29c0d2adafd1b6b3721dcbc9a70
Insu Park <facessu@gmail.com>
2023-08-02 23:18:20 +0900

    Programmers Day 20 - Stack and Queue (level 2 프로세스) complete

--- Programmers/DayTwentyOneStackAndQueue.playground/Pages/프로세스.xcplaygroundpage/Contents.swift
+++ Programmers/DayTwentyOneStackAndQueue.playground/Pages/프로세스.xcplaygroundpage/Contents.swift
@@ -7,17 +7,23 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
         return ForPriority(element: $0.element,
                            isFindValue: $0.offset == location)
     }
-    
+
     var result: [ForPriority] = []
-    
+
     while result.count != priorities.count {
         for (idx, newPriority) in newPriorities.enumerated() {
             let checkPriorities: Bool = newPriorities.filter { $0.element > newPriority.element }.count == 0
-            
+
             if checkPriorities {
                 // 우선순위가 더 높은 친구가 없다. == 프로세스를 실행해도 된다.
                 newPriority.priorityOrder = result.count
-                result.append(newPriorities.removeFirst())
+                let findPriority = newPriorities.removeFirst()
+                result.append(findPriority)
+
+                if findPriority.isFindValue {
+                    return findPriority.priorityOrder + 1
+                }
+
                 break
             } else {
                 // 우선순위가 더 높은 친구가 내부에 있다. == 프로세스를 First Out Last In 한다.
@@ -27,14 +33,14 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
         }
     }
 
-    return result.filter { $0.isFindValue }.first!.priorityOrder + 1
+    return 0
 }
 
 class ForPriority {
     let element: Int
     let isFindValue: Bool
     var priorityOrder: Int = -1
-    
+
     init(element: Int, isFindValue: Bool) {
         self.element = element
         self.isFindValue = isFindValue
