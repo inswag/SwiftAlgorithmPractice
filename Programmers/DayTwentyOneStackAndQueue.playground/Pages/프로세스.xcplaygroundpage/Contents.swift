ad2af199ef3661bcee58f45fdb0fe8d110e568be
Insu Park <facessu@gmail.com>
2023-08-02 23:14:09 +0900

    Programmers Day 20 - Stack and Queue (level 2 프로세스) resolving... (accuracy problem)

---
+++ Programmers/DayTwentyOneStackAndQueue.playground/Pages/프로세스.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var newPriorities = priorities.enumerated().map {
+        return ForPriority(element: $0.element,
+                           isFindValue: $0.offset == location)
+    }
+    
+    var result: [ForPriority] = []
+    
+    while result.count != priorities.count {
+        for (idx, newPriority) in newPriorities.enumerated() {
+            let checkPriorities: Bool = newPriorities.filter { $0.element > newPriority.element }.count == 0
+            
+            if checkPriorities {
+                // 우선순위가 더 높은 친구가 없다. == 프로세스를 실행해도 된다.
+                newPriority.priorityOrder = result.count
+                result.append(newPriorities.removeFirst())
+                break
+            } else {
+                // 우선순위가 더 높은 친구가 내부에 있다. == 프로세스를 First Out Last In 한다.
+                let first = newPriorities.removeFirst()
+                newPriorities.append(first)
+            }
+        }
+    }
+
+    return result.filter { $0.isFindValue }.first!.priorityOrder + 1
+}
+
+class ForPriority {
+    let element: Int
+    let isFindValue: Bool
+    var priorityOrder: Int = -1
+    
+    init(element: Int, isFindValue: Bool) {
+        self.element = element
+        self.isFindValue = isFindValue
+    }
+}
+
+solution([2,1,3,2], 2)
+solution([1,1,9,1,1,1], 0)
