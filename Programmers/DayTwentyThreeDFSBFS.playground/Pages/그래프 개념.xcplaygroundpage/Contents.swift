d7622cbda7f2cbfa7da43b43908d1a04a61503d6
Insu Park <facessu@gmail.com>
2023-08-09 23:31:07 +0900

    Programmers Day 23 - Coding tests for high grade, BFS/DFS (그래프 개념)

---
+++ Programmers/DayTwentyThreeDFSBFS.playground/Pages/그래프 개념.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ - 그래프 개념
+  * Node와 Edge로 구성되는 자료구조
+ - Node란?
+  * 노드는 정점으로 데이터가 저장된다.
+ - Edge란?
+  * 간선 or Branch 라고도 불리며 Node 를 연결한 선이다.
+ - 인접 Node
+  * Edge 에 의해 직접 연결된 Node.
+ 
+ - 그래프 종류
+  * 방향 그래프
+   -> Edge에 방향이 존재하여 방향으로만 갈 수 있음. 단방향
+  * 무방향 그래프
+   -> Edge에 방향이 존재하지 않아 양방향으로 갈 수 있음. 양방향
+ */
